626a71d237d5b5d2790c4ba4fae19de8021f98c1
Ktoxsyrov <ktoxsyrovv@inbox.ru>
2022-06-20 11:05:44 +0300

    file

--- app/src/main/java/com/example/ht5/MainActivity.kt
+++ app/src/main/java/com/example/ht5/MainActivity.kt
@@ -1,53 +1,45 @@
 package com.example.ht5
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.view.isVisible
-import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ht5.adapter.HeroesAdapter
 import com.example.ht5.model.Hero
-import com.example.ht5.network.Repository
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import kotlinx.coroutines.awaitAll
 import okhttp3.Call
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import java.io.IOException
+import java.io.*
 import java.lang.Thread.sleep
 
 private lateinit var heroRecyclerAdapter: HeroesAdapter
 
-
 interface OnItemClickListener{
     fun onItemClicked(position: Int, view: View)
 }
-
 
 class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
-       // val loading = findViewById<ProgressBar>(R.id.loading)
 
         val heroes = mutableListOf<Hero>()
-        Repository().makeApiCall(heroes)
+
+
+
+        makeApiCall(heroes)
         println(heroes.size)
 
-            //если не подождать, то список героев прикрепляется пустым, а потом наполняется
-        sleep(3000) //жду 3 сек пока репа получит ответ. надо было делать синхронное выполнение. на неделе асинхрона уделить внимание
-        println("==============================================================================================")
-        if (heroes.size == 0) Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show()
+        while(heroes.size==0)
+            sleep(100)
 
         val recyclerView = findViewById<RecyclerView>(R.id.heroesRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -55,14 +47,9 @@ class MainActivity : AppCompatActivity()  {
         heroRecyclerAdapter.setHeroList(heroes)
         recyclerView.adapter = heroRecyclerAdapter
 
-
         recyclerView.addOnItemCLickListener(object: OnItemClickListener{
             override fun onItemClicked(position: Int, view: View) {
                 val clickedHero = heroRecyclerAdapter.getHeroByPos(position)
-               // Toast.makeText(this@MainActivity, clickedHero.localized_name, Toast.LENGTH_SHORT).show()
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.containerForDetails, DetailsFragment(clickedHero))
-//                    .commit()
 
                 val intent = Intent(this@MainActivity, HeroDetailsActivity::class.java)
                 intent.putExtra("name", clickedHero.localized_name)
@@ -81,7 +68,6 @@ class MainActivity : AppCompatActivity()  {
         })
     }
 
-
     private fun RecyclerView.addOnItemCLickListener(onClickListener: OnItemClickListener){
         this.addOnChildAttachStateChangeListener(object: RecyclerView.OnChildAttachStateChangeListener {
             override fun onChildViewDetachedFromWindow(view: View) {
@@ -94,5 +80,66 @@ class MainActivity : AppCompatActivity()  {
                 }
             }
         })
+    }
+
+    private val URL = "https://api.opendota.com/api/heroStats"
+    private val request = Request.Builder().url(URL).build()
+    private val okHttpClient = OkHttpClient()
+
+    private fun makeApiCall(heroesList: MutableList<Hero>?) {
+        val dataFromFile = readFile()
+        if (dataFromFile != null) {
+            println("From file")
+            val moshi = Moshi.Builder().build()
+            val listHero = Types.newParameterizedType(List::class.java, Hero::class.java)
+            val jsonAdapter: JsonAdapter<List<Hero>> = moshi.adapter(listHero)
+            val heroes = jsonAdapter.fromJson(dataFromFile!!)
+            println(heroes?.get(15))
+            for (i in 0 until heroes!!.size) {
+                heroesList?.add(heroes[i])
+               // println(heroesList?.get(i))
+            }
+        } else {
+            println("From network")
+            okHttpClient.newCall(request).enqueue(object : okhttp3.Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    //ничего
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body?.string()
+                    writeFile(body)
+                    println(body)
+                    val moshi = Moshi.Builder().build()
+                    val listHero = Types.newParameterizedType(List::class.java, Hero::class.java)
+                    val jsonAdapter: JsonAdapter<List<Hero>> = moshi.adapter(listHero)
+                    val heroes = jsonAdapter.fromJson(body!!)
+                    println(heroes?.get(15))
+                    for (i in 0 until heroes!!.size) {
+                        heroesList?.add(heroes[i])
+                       // println(heroesList?.get(i))
+                    }
+
+                }
+            })
+        }
+    }
+
+    private fun writeFile(json: String?){
+        try{
+            val bw = BufferedWriter(OutputStreamWriter(openFileOutput("dotaHeroesFile", MODE_PRIVATE)))
+            bw.write(json)
+            bw.close()
+        } catch (e: FileNotFoundException){
+            println("Файл не найден")
+        }
+    }
+    private fun readFile():String?{
+        try {
+            val br = BufferedReader(InputStreamReader(openFileInput("dotaHeroesFile")))
+            return br.readText()
+        }catch (e: FileNotFoundException){
+            return null
+        }
     }
 }
